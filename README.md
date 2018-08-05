1682a8c1bd454e33da13b551fac09b11c7d82673
mshvern <maksym.shvernenko@gmail.com>
2018-08-06 00:36:52 +0300

    ShareJS fresh start

--- README.md
+++
@@ -1,4 +0,0 @@
-Copad aims to become a useful portable tool for collaborative code editing and execution. </br>
-Planned languages: 
-*Python
-*Javascript
--- tests/main.js
+++ tests/main.js
@@ -1,9 +1,9 @@
 import assert from "assert";
 
-describe("meteor", function () {
+describe("copad", function () {
   it("package.json has correct name", async function () {
     const { name } = await import("../package.json");
-    assert.strictEqual(name, "meteor");
+    assert.strictEqual(name, "copad");
   });
 
   if (Meteor.isClient) {
