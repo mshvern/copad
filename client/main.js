0fdc0e95a2a54f26fb215e1c3a03fb8ec893d829
mshvern <maksym.shvernenko@gmail.com>
2018-08-06 02:40:00 +0300

    something that works! codemirror from perak

--- client/main.js
+++ client/main.js
@@ -1,24 +1,20 @@
 import { Template } from 'meteor/templating'
-import { ReactiveVar } from 'meteor/reactive-var'
 import { Meteor } from 'meteor/meteor'
 import './main.html'
 import { Codespaces } from '../imports/api/codespaces'
-if (Meteor.isClient) {
-  Template.editor.helpers({
-    docid: function () {
-      var doc = Codespaces.findOne()
-      if (doc) {
-        return doc._id
-      } else {
-        return undefined
-      }
-    },
-    config: function () {
-      return space => {
-        space.setOption('lineNumbers', true)
-        space.setOption('mode', 'javascript')
-        space.setOption('theme', 'monokai')
-      }
+
+Template.editor.helpers({
+
+  'editorOptions': function () {
+    return {
+      lineNumbers: true,
+      theme: 'blackboard',
+      mode: 'javascript'
     }
-  })
-}
+  },
+
+  'editorCode': function () {
+    return 'Copad!'
+  }
+
+})
