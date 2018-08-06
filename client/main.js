2bcf8039faf4f4d730ef91ef1dcb614b5c59228c
mshvern <maksym.shvernenko@gmail.com>
2018-08-06 03:50:24 +0300

    Nothing works with meteor, switching to plain js for codemirror

--- client/main.js
+++ client/main.js
@@ -2,19 +2,19 @@ import { Template } from 'meteor/templating'
 import { Meteor } from 'meteor/meteor'
 import './main.html'
 import { Codespaces } from '../imports/api/codespaces'
-
-Template.editor.helpers({
-
-  'editorOptions': function () {
-    return {
-      lineNumbers: true,
-      theme: 'blackboard',
-      mode: 'javascript'
-    }
-  },
-
-  'editorCode': function () {
-    return 'Copad!'
+if (Meteor.isServer) {
+  if (!Codespaces.findOne()) {
+    Codespaces.insert({'source': "console.log('Hello, Copad!')"})
   }
+}
 
-})
+Template.editor.rendered = function () {
+  console.log('rendered!')
+  let editor = CodeMirror.fromTextArea(this.find('#codearea'), {
+    lineNumbers: true,
+    mode: 'javascript',
+    theme: 'blackboard'
+  })
+  editor.getDoc().setValue('var msg = "Hi";')
+  editor.on('change', () => console.log('callback from change'))
+}
